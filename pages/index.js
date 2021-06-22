bad4eb34d89c2abdc55177d90cff6372e1bc3520
Maurici Ferreira Junior <m.f.j@hotmail.com>
2021-06-22 00:29:10 -0300

    Added social media bar

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,11 @@ export default function Home() {
                 <div className={styles.profile_description}>
                     <h1 className={styles.profile_description_headline}>Hi, I'm Maurici</h1>
                     <p className={styles.profile_description_}>Web and mobile developer</p>
+                    <br/>
+                    <div class={styles.social_mediar_bar}>
+                        <a href="https://www.linkedin.com/in/mauricifj/" class="linkedin" target="_blank"><i class="fa fa-linkedin"></i></a>
+                        <a href="https://github.com/mauricifj/" class="github" target="_blank"><i class="fa fa-github-square"></i></a>
+                    </div>
                 </div>
             </main>
         </div>
